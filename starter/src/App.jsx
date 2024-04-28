1ff5ea776d4e82ea62344e745675bf1f43ab7bd0
Amadou-Marong <marongamadou6@gmail.com>
2024-04-28 15:46:53 +0000

    reviews project

---
+++ starter/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import people from './data'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa6";
+
+const App = () => {
+  const [index, setIndex] = useState(0)
+
+  const {name, job, image, text} = people[index]
+
+  // console.log(1 % 4);
+  // console.log(2 % 4);
+  // console.log(3 % 4);
+  // console.log(4 % 4);
+  // console.log(10 % 4);
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1
+      if(newIndex < 0){
+        newIndex = people.length - 1
+      }
+      return newIndex
+    })
+  }
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1
+      if(newIndex > people.length - 1){
+        newIndex = 0
+      }
+      return newIndex
+    })
+  }
+
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img"/>
+          <span className="quote-icon">
+            <FaQuoteRight/>
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="button-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+      </article>
+    </main>
+  )
+};
+export default App;
